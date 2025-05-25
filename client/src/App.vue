f740b6fa98c32b743d46d430a3ad9b04434b53b3
ant <aneubert@mobol.net>
2025-05-25 23:50:01 +0200

    Added floating navigation drawer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,37 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
   <v-layout>
     <v-app>
-      <v-app-bar title="Note taker"></v-app-bar>
+      <v-app-bar>
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
+        <v-app-bar-title>Note taker</v-app-bar-title>
+      </v-app-bar>
 
-      <v-navigation-drawer>
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      >
         <v-list nav>
           <v-list-item title="Navigation drawer" link></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
       <v-main>
-        <v-container
-          fluid
-        >
+        <v-container fluid>
           <RouterView />
         </v-container>
       </v-main>
     </v-app>
   </v-layout>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
+
+const drawer = ref(false)
+</script>
 
 <style scoped lang="scss">
 @use "./assets/base.scss" as *;
